e4a46fc4d214ae20955dc0f1342476fb737b59af
ShohagHossen <mdshohaghossen29@gmail.com>
2023-08-26 07:17:39 -0700

    assignment-04

---
+++ index.js
@@ -0,0 +1,45 @@
+// 1.checkDataTypes Start 
+function checkDataTypes() {
+
+}
+checkDataTypes()
+
+
+
+
+// 1.checkDataTypes Close
+
+
+// 2.checkLeapYear Start
+function checkLeapYear(year) {
+    if (year % 4 == 0 && year % 100 != 0) {
+        console.log(year + "is a Leap Year")
+    } else if (year % 400 == 0) {
+        console.log(year + " is a Leap Year")
+    } else {
+        console.log(year + " is not a Leap Year")
+    }
+}
+checkLeapYear(2023);
+
+// 2.checkLeapYear Close
+
+// 4.findLargest Start
+function findLargest(p, q, r) {
+    if (p >= q && p=>= r) {
+        return p; {
+            else if () {
+
+            }
+        }
+
+    }
+}
+var a = prompt("Please enter the first number");
+var b = prompt("Please enter the first number");
+var c = prompt("Please enter the first number");
+var x = findLargest(a, b, c);
+console.log('${a},${b},${c}: Largest = ${ x }');
+
+// 4.findLargest Close
+
